a8dcdf7d98c49d84bb11ce6b7517e47bf7cefb4e
Harsha Amarasinghe <amarasinghe.ehm@MOF.local>
2023-08-16 12:00:11 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,78 @@
+
+
+<template>
+  
+  
+  <div class="flexbox-container flex">
+<!-- Left panel -->
+    <div class="flexbox-item flexbox-item-1 w-1/2 h-screen bg-[#2B6CB0]"> 
+      
+        
+        
+      <div class="flexbox-item flexbox-item-1 h-72 "><div class="img px-52 mt-8"><img src="../src/assets/logo.PNG"></div></div> 
+        <div class="flexbox-item flexbox-item-2 h-80"><div class="h1 text-white text-6xl font-bold  px-52">Start making your dreams come true</div>
+        <br>
+        <div class="p text-white text-xl  px-52">Create an account and discover the worlds' best UI component framework.</div>
+        <div class="img px-52"><img src="../src/assets/Capture.PNG"></div>
+      
+      </div>
+        <div class="flexbox-item flexbox-item-3"><div class="h1 text-white text-small mt-10  px-52 absolute bottom-[20px]">© 2022 Chakra UI. All rights reserved.</div></div>
+      </div>
+<!-- Right panel -->
+    <div class="flexbox-item flexbox-item-2 w-1/2 h-screen bg-[#ffffff]">
+      <div class="flexbox-item flexbox-item-1 h-60 "></div>
+      <div class="p text-4xl font-bold px-48 text-center ">Log in to your account</div> 
+    
+      <div class="flexbox-item flexbox-item-2  ">
+      <div class="p text-xl px-44 text-center">Don't have an account? Sign up</div>
+    
+    </div>
+
+    <!-- Form -->
+    <form class="bg-white rounded pl-56 pr-64 mt-10 ">
+    <div class="mb-4">
+      <label class="block text-gray-700  mb-2" for="email">
+        Email
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Enter your email">
+    </div>
+    <div class="mb-6">
+      <label class="block text-gray-700 mb-2" for="password">
+        Password
+      </label>
+      <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+      
+    </div>
+
+    <div class="a text-left">
+  <label>
+  <input type="checkbox" checked> Remember me 
+</label>
+</div>
+<div class="b text-right">
+<a class="inline-block  text-blue-800 hover:text-blue-800 mb-5 mt--20 text-right" href="#">
+        Forgot Password?
+      </a>
+    </div>
+
+  
+    
+    <button class="bg-blue-500 hover:bg-blue-700 rounded-lg text-white font-bold py-2 px-4 focus:outline-none focus:shadow-outline w-full" type="button">
+        Sign In
+      </button>
+      <button class=" hover:bg-gray-100  py-2 px-4 rounded-lg focus:outline-none focus:shadow-outline w-full mt-5 border-2" type="button">
+        Sign In with Google
+      </button>
+  </form>
+      
+      
+      
+      
+    </div>
+  </div>
+
+  
+
+</template>
+
+
